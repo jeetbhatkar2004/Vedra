3becacb0550d3ea45ac43d2f2fb7daf10352ca45
raditya2023F <adityarana2003.ar@gmail.com>
2025-09-10 16:02:20 -0400

    Fixed fine-print over DataCite.

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -221,7 +221,7 @@ const About: React.FC = () => {
                Our Role in the DataCite Community
                <span className="relative group">
                  <span className="align-super text-lg text-vedra-calpoly font-bold ml-1 cursor-help">*</span>
-                 <div className="absolute bottom-full left-1/2 transform -translate-x-1/2 mb-2 px-3 py-2 bg-vedra-night text-white text-sm rounded-lg opacity-0 group-hover:opacity-100 transition-opacity duration-200 pointer-events-none whitespace-nowrap z-10">
+                 <div className="absolute bottom-full left-1/2 transform -translate-x-1/2 mb-2 px-3 py-2 bg-vedra-night text-white text-sm rounded-lg opacity-0 group-hover:opacity-100 transition-opacity duration-200 pointer-events-none z-10 max-w-xs sm:max-w-sm md:max-w-md lg:whitespace-nowrap lg:max-w-none">
                    We are in the final stages of securing approval from DataCite's Board and look forward to sharing updates soon.
                    <div className="absolute top-full left-1/2 transform -translate-x-1/2 w-0 h-0 border-l-4 border-r-4 border-t-4 border-transparent border-t-vedra-night"></div>
                  </div>
